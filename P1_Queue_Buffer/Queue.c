ea6d8df8917af7ddab2b6f692b9021239aac193c
JennR <jenniferreynagacabrera@gmail.com>
2024-11-14 17:30:37 -0600

    Add the makefile configuration

--- P1_Queue_Buffer/Queue.c
+++ P1_Queue_Buffer/Queue.c
@@ -9,7 +9,6 @@
 /*----------------------------------------------------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 #include <string.h>
 #include "Queue.h"
 
