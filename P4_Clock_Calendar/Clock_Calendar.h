32082fc1c7ba6841f854e1046ce4a24a393912ab
JennR <jenniferreynagacabrera@gmail.com>
2024-11-18 16:27:35 -0600

    Add a brief to each function

--- P4_Clock_Calendar/Clock_Calendar.h
+++ P4_Clock_Calendar/Clock_Calendar.h
@@ -17,44 +17,44 @@
 /*----------------------------------------------------------------------------*/
 /*                             Defines and macros                             */
 /*----------------------------------------------------------------------------*/
-#define DISABLE                 0
-#define ENABLE                  1
-#define DEFAULT_VAL             0
+#define DISABLE                 0u
+#define ENABLE                  1u
+#define DEFAULT_VAL             0u
 
 /* Time Max and Min */
 /* Note: All time parameters have the same min val */
-#define MIN_TIME                0
-#define MAX_MIN                 59
-#define MAX_SEC                 59
-#define MAX_HOUR                23
+#define MIN_TIME                0u
+#define MAX_MIN                 59u
+#define MAX_SEC                 59u
+#define MAX_HOUR                23u
 
 /* Date Max and Min */
-#define MIN_DAY                 1
-#define MAX_DAY                 31
-#define MIN_WDAY                0       /* 0 == SUNDAY */
-#define MAX_WDAY                6       /* 6 == SATURDAY */
-#define RESET_WDAY              1       /* 1 == MONDAY */
-#define MIN_MONTH               1
-#define MAX_MONTH               12
-#define MIN_YEAR                1900
-#define MAX_YEAR                2100
+#define MIN_DAY                 1u
+#define MAX_DAY                 31u
+#define MIN_WDAY                0u       /* 0 == SUNDAY */
+#define MAX_WDAY                6u       /* 6 == SATURDAY */
+#define RESET_WDAY              1u       /* 1 == MONDAY */
+#define MIN_MONTH               1u
+#define MAX_MONTH               12u
+#define MIN_YEAR                1900u
+#define MAX_YEAR                2100u
 
 /* For increment Time and Date Values*/
-#define SECOND                  1
-#define MINUTE                  1
-#define HOUR                    1
-#define MONTH                   1
-#define YEAR                    1
+#define SECOND                  1u
+#define MINUTE                  1u
+#define HOUR                    1u
+#define MONTH                   1u
+#define YEAR                    1u
 
 /* Days for Months */
-#define THIRTY_ONE              31
-#define THIRTY                  30
-#define TWENTY_EIGHT            28
-#define TWENTY_NINE             29
+#define THIRTY_ONE              31u
+#define THIRTY                  30u
+#define TWENTY_EIGHT            28u
+#define TWENTY_NINE             29u
 
 /* MONTHS to evaluate */
-#define JANUARY                 1
-#define FEBRUARY                2
+#define JANUARY                 1u
+#define FEBRUARY                2u
 
 
 
@@ -62,36 +62,34 @@
 /*                                 Data types                                 */
 /*----------------------------------------------------------------------------*/
 
-/*  The RTCC should have a control union structure to handle certain number of 
-flags each of them represented by a single bit */
+/* Control union structure to handle various flags, each represented by a single bit */
 typedef union _AppRtcc_Ctrl
 {
-    uint8_t Register;   /*8 bit vaiable*/
+    uint8_t Register;   /* 8-bit variable */
     struct _bits
     {
-        uint8_t clk_en : 1;         /*!< clock enable flag      */
-        uint8_t al_set : 1;         /*!< alarm set flag         */
-        uint8_t al_active : 1;      /*!< alarm active flag      */
-        uint8_t none : 4;
+        uint8_t clk_en : 1;         /*!< Clock enable flag      */
+        uint8_t al_set : 1;         /*!< Alarm set flag         */
+        uint8_t al_active : 1;      /*!< Alarm active flag      */
+        uint8_t none : 4;           /*!< Unused bits            */
     } bits;
 } AppRtcc_Ctrl;
 
-/* The RTCC should have the following control structure which will be passed 
-as parameter on each interface, making no necessary the usage of global variables  */
-
+/* Control structure for the RTCC, passed as a parameter in each interface, 
+   eliminating the need for global variables */
 typedef struct _AppRtcc_Clock 
 {
-    uint8_t tm_sec;             /*!< seconds,  range 0 to 59          */
-    uint8_t tm_min;             /*!< minutes, range 0 to 59           */
-    uint8_t tm_hour;            /*!< hours, range 0 to 23             */
-    uint8_t tm_mday;            /*!< day of the month, range 1 to 31  */
-    uint8_t tm_mon;             /*!< month, range 1 to 12             */
-    uint16_t tm_year;           /*!< years in rage 1900 2100          */
-    uint8_t tm_wday;            /*!< day of the week, range 0 to 6    */
-    uint8_t al_min;             /*!< alarm minutes, range 0 to 59     */
-    uint8_t al_hour;            /*!< alarm hours, range 0 to 23       */
-    uint8_t mt_days[13u];       /*!< max days on each month */
-    AppRtcc_Ctrl ctrl;          /*!< clock control bits */
+    uint8_t tm_sec;             /*!< Seconds, range 0 to 59          */
+    uint8_t tm_min;             /*!< Minutes, range 0 to 59           */
+    uint8_t tm_hour;            /*!< Hours, range 0 to 23             */
+    uint8_t tm_mday;            /*!< Day of the month, range 1 to 31  */
+    uint8_t tm_mon;             /*!< Month, range 1 to 12             */
+    uint16_t tm_year;           /*!< Year, range 1900 to 2100         */
+    uint8_t tm_wday;            /*!< Day of the week, range 0 to 6    */
+    uint8_t al_min;             /*!< Alarm minutes, range 0 to 59     */
+    uint8_t al_hour;            /*!< Alarm hours, range 0 to 23       */
+    uint8_t mt_days[13u];       /*!< Maximum days in each month       */
+    AppRtcc_Ctrl ctrl;          /*!< Clock control bits               */
 } AppRtcc_Clock;
 
 /*----------------------------------------------------------------------------*/
@@ -103,90 +101,152 @@ typedef struct _AppRtcc_Clock
 /*----------------------------------------------------------------------------*/
 /*                           Declaration of functions                         */
 /*----------------------------------------------------------------------------*/
-
 /**
  * @brief  Interface to initialize the internal clock structure
-*/
-void AppRtcc_clockInit( AppRtcc_Clock *rtcc );
+ * 
+ * @param rtcc Pointer to the RTCC clock structure
+ */
+void AppRtcc_clockInit(AppRtcc_Clock *rtcc);
 
 /**
  * @brief   Interface to set a new time
-*/
-uint8_t AppRtcc_setTime( AppRtcc_Clock *rtcc, uint8_t hour, uint8_t minutes, uint8_t seconds );
+ * 
+ * @param rtcc Pointer to the RTCC clock structure
+ * @param hour Hour value to set
+ * @param minutes Minute value to set
+ * @param seconds Second value to set
+ * @return uint8_t Status of the time set operation (TRUE if successful, FALSE otherwise)
+ */
+uint8_t AppRtcc_setTime(AppRtcc_Clock *rtcc, uint8_t hour, uint8_t minutes, uint8_t seconds);
 
 /**
  * @brief   Interface to set a new date
-*/
-uint8_t AppRtcc_setDate( AppRtcc_Clock *rtcc, uint8_t day, uint8_t month, uint16_t year );
+ * 
+ * @param rtcc Pointer to the RTCC clock structure
+ * @param day Day value to set
+ * @param month Month value to set
+ * @param year Year value to set
+ * @return uint8_t Status of the date set operation (TRUE if successful, FALSE otherwise)
+ */
+uint8_t AppRtcc_setDate(AppRtcc_Clock *rtcc, uint8_t day, uint8_t month, uint16_t year);
 
 /**
  * @brief   Interface to set a new alarm value
-*/
-uint8_t AppRtcc_setAlarm( AppRtcc_Clock *rtcc, uint8_t hour, uint8_t minutes );
+ * 
+ * @param rtcc Pointer to the RTCC clock structure
+ * @param hour Hour value to set for the alarm
+ * @param minutes Minute value to set for the alarm
+ * @return uint8_t Status of the alarm set operation (TRUE if successful, FALSE otherwise)
+ */
+uint8_t AppRtcc_setAlarm(AppRtcc_Clock *rtcc, uint8_t hour, uint8_t minutes);
 
 /**
  * @brief   Interface to read the current time
-*/
-void AppRtcc_getTime( AppRtcc_Clock *rtcc, uint8_t *hour, uint8_t *minutes, uint8_t *seconds );
+ * 
+ * @param rtcc Pointer to the RTCC clock structure
+ * @param hour Pointer to store the current hour value
+ * @param minutes Pointer to store the current minute value
+ * @param seconds Pointer to store the current second value
+ */
+void AppRtcc_getTime(AppRtcc_Clock *rtcc, uint8_t *hour, uint8_t *minutes, uint8_t *seconds);
 
 /**
  * @brief    Interface to read the current date
-*/
-void AppRtcc_getDate( AppRtcc_Clock *rtcc, uint8_t *day, uint8_t *month, uint16_t *year, uint8_t *weekDay );
+ * 
+ * @param rtcc Pointer to the RTCC clock structure
+ * @param day Pointer to store the current day value
+ * @param month Pointer to store the current month value
+ * @param year Pointer to store the current year value
+ * @param weekDay Pointer to store the current weekday value
+ */
+void AppRtcc_getDate(AppRtcc_Clock *rtcc, uint8_t *day, uint8_t *month, uint16_t *year, uint8_t *weekDay);
 
 /**
  * @brief   Interface to read the current alarm
-*/
-uint8_t AppRtcc_getAlarm( AppRtcc_Clock *rtcc, uint8_t *hour, uint8_t *minutes );
+ * 
+ * @param rtcc Pointer to the RTCC clock structure
+ * @param hour Pointer to store the current alarm hour value
+ * @param minutes Pointer to store the current alarm minute value
+ * @return uint8_t Status of the alarm read operation (TRUE if successful, FALSE otherwise)
+ */
+uint8_t AppRtcc_getAlarm(AppRtcc_Clock *rtcc, uint8_t *hour, uint8_t *minutes);
 
 /**
- * @brief   Interface  to clear an active current alarm
-*/
-void AppRtcc_clearAlarm( AppRtcc_Clock *rtcc );
+ * @brief   Interface to clear an active current alarm
+ * 
+ * @param rtcc Pointer to the RTCC clock structure
+ */
+void AppRtcc_clearAlarm(AppRtcc_Clock *rtcc);
 
 /**
  * @brief   Interface to get the flag that indicates if the alarm is active
-*/
-uint8_t AppRtcc_getAlarmFlag( AppRtcc_Clock *rtcc );
+ * 
+ * @param rtcc Pointer to the RTCC clock structure
+ * @return uint8_t Status of the alarm flag (TRUE if active, FALSE otherwise)
+ */
+uint8_t AppRtcc_getAlarmFlag(AppRtcc_Clock *rtcc);
 
 /**
  * @brief   Interface to Clock Periodic function
-*/
-void AppRtcc_periodicTask( AppRtcc_Clock *rtcc );
+ * 
+ * @param rtcc Pointer to the RTCC clock structure
+ */
+void AppRtcc_periodicTask(AppRtcc_Clock *rtcc);
 
 /**
  * @brief   Interface to update Time values
-*/
-uint8_t AppRtcc_updateTime( AppRtcc_Clock *rtcc);
+ * 
+ * @param rtcc Pointer to the RTCC clock structure
+ * @return uint8_t Status of the time update operation (TRUE if successful, FALSE otherwise)
+ */
+uint8_t AppRtcc_updateTime(AppRtcc_Clock *rtcc);
 
 /**
  * @brief   Interface to update Date values
-*/
-void AppRtcc_updateDate( AppRtcc_Clock *rtcc, uint8_t update_day);
+ * 
+ * @param rtcc Pointer to the RTCC clock structure
+ * @param update_day Day value to update
+ */
+void AppRtcc_updateDate(AppRtcc_Clock *rtcc, uint8_t update_day);
 
 /**
- * @brief   Interface to reset/set initial values of date and Time
-*/
-void AppRtcc_initDateTime( AppRtcc_Clock *rtcc);
+ * @brief   Interface to reset/set initial values of date and time
+ * 
+ * @param rtcc Pointer to the RTCC clock structure
+ */
+void AppRtcc_initDateTime(AppRtcc_Clock *rtcc);
 
 /**
  * @brief   Interface to evaluate if the year is leap or not
-*/
-uint8_t AppRtcc_leapYear( uint16_t year);
+ * 
+ * @param year Year value to evaluate
+ * @return uint8_t Status indicating if the year is leap (TRUE if leap year, FALSE otherwise)
+ */
+uint8_t AppRtcc_leapYear(uint16_t year);
 
 /**
  * @brief   Interface to set MAX Days for each month
-*/
-void AppRtcc_maxDaysMonths( AppRtcc_Clock *rtcc);
+ * 
+ * @param rtcc Pointer to the RTCC clock structure
+ */
+void AppRtcc_maxDaysMonths(AppRtcc_Clock *rtcc);
 
 /**
- * @brief   Interface to give MAX Days depending the month
-*/
-uint8_t AppRtcc_maxDaysMonth( AppRtcc_Clock *rtcc, uint8_t month, uint8_t leap);
+ * @brief   Interface to give MAX Days depending on the month
+ * 
+ * @param rtcc Pointer to the RTCC clock structure
+ * @param month Month value to evaluate
+ * @param leap Status indicating if the year is leap
+ * @return uint8_t Maximum days in the specified month
+ */
+uint8_t AppRtcc_maxDaysMonth(AppRtcc_Clock *rtcc, uint8_t month, uint8_t leap);
 
 /**
  * @brief   Interface to indicate if the Alarm is Active
-*/
-void AppRtcc_alarmStatus( AppRtcc_Clock *rtcc);
+ * 
+ * @param rtcc Pointer to the RTCC clock structure
+ */
+void AppRtcc_alarmStatus(AppRtcc_Clock *rtcc);
+
 
 #endif /* CLOCK_CALENDARS_H_ */
