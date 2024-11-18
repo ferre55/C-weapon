d12170fa5c57b04e32699aae6e104bb68eb9838b
JennR <jenniferreynagacabrera@gmail.com>
2024-11-18 09:23:47 -0600

    Add briefs for each function

--- P1_Queue_Buffer/Queue.h
+++ P1_Queue_Buffer/Queue.h
@@ -25,7 +25,7 @@
 /*----------------------------------------------------------------------------*/
 typedef struct
 {
-    void        *Buffer;                /*!< Pointer to array that store buffer data - 40 bits*/
+    void        *Buffer;                /*!< Pointer to array that store buffer data*/
     uint32_t    Elements;               /*!< Number of elements to store (the queue lenght) */
     uint8_t     Size;                   /*!< Size of the elements to store */
     uint8_t     Head;                   /*!< Variable to signal the next queue space to write */
@@ -41,27 +41,43 @@ typedef struct
 /*----------------------------------------------------------------------------*/
 
 /**
- * @brief Initialization function for the queue
+ * @brief Initialization function for the queue.
+ * 
+ * @param queue Pointer to the queue structure to initialize.
  */
 void AppQueue_initQueue( AppQue_Queue *queue );
 
+
 /**
  * @brief Function that allows writing an element to the queue.
+ * 
+ * @param queue Pointer to the queue structure.
+ * @param data Pointer to the data to write into the queue.
+ * @return uint8_t Status of the write operation.
  */
 uint8_t AppQueue_writeData( AppQue_Queue *queue, void *data );
 
 /**
  * @brief Function that allows reading an element from the queue.
+ * 
+ * @param queue Pointer to the queue structure.
+ * @param data Pointer to the buffer to store the read data.
+ * @return uint8_t Status of the read operation.
  */
 uint8_t AppQueue_readData( AppQue_Queue *queue, void *data );
 
 /**
  * @brief Function that indicates if the queue is empty.
+ * 
+ * @param queue Pointer to the queue structure.
+ * @return uint8_t Status indicating if the queue is empty.
  */
 uint8_t AppQueue_isQueueEmpty( AppQue_Queue *queue );
 
 /**
  * @brief Function that empties the queue.
+ * 
+ * @param queue Pointer to the queue structure.
  */
 void AppQueue_flushQueue( AppQue_Queue *queue );
 
